6426f8db3e6e9f00548f68f1186f72009c4b13b2
Sharif Ezzat <goodsharif@gmail.com>
2020-01-27 23:35:31 -0800

    readded sticky js

--- src/index.js
+++ src/index.js
@@ -35,21 +35,21 @@ $(document).ready(function() {
 
 
 
-// function sticktothetop() {
-//     var window_top = $(window).scrollTop();
-//     var top = $('#stick-here').offset().top;
-//     if (window_top > top) {
-//         $('#stickThis').addClass('stick');
-//         $('#stick-here').height($('#stickThis').outerHeight());
-//     } else {
-//         $('#stickThis').removeClass('stick');
-//         $('#stick-here').height(0);
-//     }
-// }
-// $(function() {
-//     $(window).scroll(sticktothetop);
-//     sticktothetop();
-// });
+function sticktothetop() {
+    var window_top = $(window).scrollTop();
+    var top = $('#stick-here').offset().top;
+    if (window_top > top) {
+        $('#stickThis').addClass('stick');
+        $('#stick-here').height($('#stickThis').outerHeight());
+    } else {
+        $('#stickThis').removeClass('stick');
+        $('#stick-here').height(0);
+    }
+}
+$(function() {
+    $(window).scroll(sticktothetop);
+    sticktothetop();
+});
 
 
 
